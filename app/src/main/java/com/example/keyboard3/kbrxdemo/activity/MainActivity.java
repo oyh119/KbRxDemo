d4dd1f086f239ab005c1bd0503f98960bd48120b
keyboard3 <1486416565@qq.com>
2016-08-16 10:24:00 +0800

    对reactivenetwork测试observeNetworkConnectivity

--- app/src/main/java/com/example/keyboard3/kbrxdemo/activity/MainActivity.java
+++ app/src/main/java/com/example/keyboard3/kbrxdemo/activity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.keyboard3.kbrxdemo.activity;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -8,6 +11,8 @@ import com.example.keyboard3.kbrxdemo.R;
 import com.example.keyboard3.kbrxdemo.core.MainPresenter;
 import com.example.keyboard3.kbrxdemo.subscribers.SubscriberOnNextListener;
 import com.example.model.Subject;
+import com.github.pwittchen.reactivenetwork.library.Connectivity;
+import com.github.pwittchen.reactivenetwork.library.ReactiveNetwork;
 import com.jakewharton.rxbinding.view.RxView;
 
 import java.util.List;
@@ -15,6 +20,8 @@ import java.util.concurrent.TimeUnit;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends BaseActivity {
 
@@ -33,7 +40,21 @@ public class MainActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         init();
+        initMonitorNetwork();
         bindView();
+    }
+
+    /**
+     * 监听网络变化
+     */
+    private void initMonitorNetwork() {
+        ReactiveNetwork.observeNetworkConnectivity(getApplicationContext())
+                .subscribeOn(Schedulers.io())
+                .filter(Connectivity.hasState(NetworkInfo.State.CONNECTED))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(connectivity -> {
+                    Log.d("keyboard3","已连接的网络类型："+connectivity.toString());
+                });
     }
 
     private void init() {
