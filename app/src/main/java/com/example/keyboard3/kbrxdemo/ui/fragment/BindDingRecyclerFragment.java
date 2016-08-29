364ca32f48c37373b763f87f8c4e027d43e0a1a6
keyboard3 <1486416565@qq.com>
2016-08-29 11:05:51 +0800

    采用了markzhai的databinding-adapter和cjj的recyclerViewWithFooter结合使用下拉刷新和上拉加载的逻辑。列表页实现简洁摈弃了adapter

--- app/src/main/java/com/example/keyboard3/kbrxdemo/ui/fragment/BindDingRecyclerFragment.java
+++ app/src/main/java/com/example/keyboard3/kbrxdemo/ui/fragment/BindDingRecyclerFragment.java
@@ -1,37 +1,42 @@
 package com.example.keyboard3.kbrxdemo.ui.fragment;
 
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.cjj.loadmore.DefaultFootItem;
 import com.cjj.loadmore.OnLoadMoreListener;
 import com.cjj.loadmore.RecyclerViewWithFooter;
 import com.example.keyboard3.kbrxdemo.R;
+import com.example.keyboard3.kbrxdemo.RecyclerBinding;
+import com.example.keyboard3.kbrxdemo.core.Config;
 import com.example.keyboard3.kbrxdemo.core.presenter.MoviePresenter;
 import com.example.keyboard3.kbrxdemo.core.subscribers.SubscriberOnNextListener;
 import com.example.keyboard3.kbrxdemo.model.Subject;
-import com.example.keyboard3.kbrxdemo.ui.fragment.common.BaseFragment;
-import com.zhy.adapter.recyclerview.CommonAdapter;
-import com.zhy.adapter.recyclerview.base.ViewHolder;
+import com.github.markzhai.recyclerview.SingleTypeAdapter;
 
-import java.util.LinkedList;
 import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BindDingRecyclerFragment extends BaseFragment {
+public class BindDingRecyclerFragment extends com.trello.rxlifecycle.components.support.RxFragment {
     private MoviePresenter presenter;
-    List<Subject> mDatas = new LinkedList<>();
     private RecyclerViewWithFooter mRecyclerViewWithFooter;
     private SubscriberOnNextListener getListOnNext;
     private int start = 0;
     private volatile int page = start;
     private SwipeRefreshLayout swipeRefreshLayout;
-    private CommonAdapter adapter;
+    private RecyclerBinding mBinding;
+    private SingleTypeAdapter mSingleTypeAdapter;
 
     public static BindDingRecyclerFragment newInstance() {
 
@@ -42,6 +47,18 @@ public class BindDingRecyclerFragment extends BaseFragment {
         return fragment;
     }
 
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_bind_ding_recycler, container, false);
+
+        mSingleTypeAdapter = new SingleTypeAdapter<>(getContext(), R.layout.item_bind_recycler);
+        mBinding.setLayoutManager(new LinearLayoutManager(getContext()));
+        mBinding.setAdapter(mSingleTypeAdapter);
+        return mBinding.getRoot();
+    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -49,59 +66,56 @@ public class BindDingRecyclerFragment extends BaseFragment {
         getListOnNext = new SubscriberOnNextListener<List<Subject>>() {
             @Override
             public void onNext(List<Subject> list) {
+                //数据加载成功之后 对刷新和加载的处理
                 if (swipeRefreshLayout.isRefreshing()) {
+                    Log.d(Config.LOG_TAG, "刷新:" + list.size());
                     swipeRefreshLayout.setRefreshing(false);
-                    mDatas.clear();
-                    mDatas.addAll(list);
+                    mSingleTypeAdapter.clear();
+                    mSingleTypeAdapter.addAll(list);
+                } else if (mRecyclerViewWithFooter.isLoadMoreEnable()) {
+                    //是否包含下一页 page+1
+                    page++;
+                    Log.d(Config.LOG_TAG, "加载更多:" + list.size());
+                    mSingleTypeAdapter.addAll(list);
                 }
-                if (mRecyclerViewWithFooter.isLoadMoreEnable()) {
-                    mDatas.addAll(list);
-                }
-                adapter.notifyDataSetChanged();
             }
 
             @Override
             public void onError(Throwable e) {
-
             }
         };
-
         swipeRefreshLayout = (SwipeRefreshLayout) getView().findViewById(R.id.swipeRefresh);
         swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_red_light, android.R.color.holo_blue_light, android.R.color.holo_green_light);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                //加载第一页
-                load(start);
+                if (!mRecyclerViewWithFooter.isLoadMoreEnable()) {
+                    page = start;
+                    //加载第一页
+                    load(page);
+                }
             }
         });
         mRecyclerViewWithFooter = (RecyclerViewWithFooter) getView().findViewById(R.id.rl_content);
-        adapter = new CommonAdapter<Subject>(getContext(), android.R.layout.simple_list_item_1, mDatas) {
-
-            @Override
-            protected void convert(ViewHolder holder, Subject subject, int position) {
-                holder.setText(android.R.id.text1, subject.getTitle());
-            }
-        };
-        mRecyclerViewWithFooter.setAdapter(adapter);
         mRecyclerViewWithFooter.setFootItem(new DefaultFootItem());//默认是这种
         mRecyclerViewWithFooter.setOnLoadMoreListener(new OnLoadMoreListener() {
             @Override
             public void onLoadMore() {
-                //加载更多
-                load(page++);
+                if (!swipeRefreshLayout.isRefreshing()) {
+                    //加载更多
+                    load(page + 1);
+                }
             }
         });
         //请求第一页
-        load(start);
-    }
-
-    @Override
-    protected int initLayoutId() {
-        return R.layout.fragment_bind_ding_recycler;
+        load(page);
     }
 
     protected void load(int page) {
+        Log.d(Config.LOG_TAG, "page:" + page);
+        if (page == start && !swipeRefreshLayout.isRefreshing()) {
+            swipeRefreshLayout.setRefreshing(true);
+        }
         presenter.getMovie(getListOnNext, this, page);
     }
 }
---
+++ app/src/main/java/com/example/keyboard3/kbrxdemo/ui/fragment/DataBind/SubjectViewModel.java
@@ -0,0 +1,16 @@
+package com.example.keyboard3.kbrxdemo.ui.fragment.DataBind;
+
+import com.example.keyboard3.kbrxdemo.model.Subject;
+
+/**
+ * Desc:
+ * Author: ganchunyu
+ * Date: 2016-08-29 8:47
+ */
+public class SubjectViewModel {
+    public Subject data;
+
+    public SubjectViewModel(Subject data) {
+        this.data = data;
+    }
+}
